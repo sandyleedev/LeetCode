5e99dd31fa7b5d215e88efb0a322ae7814f2deaa
butter-cloud <130043415+butter-cloud@users.noreply.github.com>
2024-10-11 20:30:02 +0900

    Time: 0 ms, Memory: 41.2 MB - LeetHub

---
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int index = -1;
+        int max = -1;
+
+        for (int i = 0; i < nums.length; i++){
+            if (max < nums[i]){
+                max = nums[i];
+                index = i;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != max && nums[i] * 2 > max){
+                index = -1;
+            }
+        }
+        
+        return index;
+    }
+}
