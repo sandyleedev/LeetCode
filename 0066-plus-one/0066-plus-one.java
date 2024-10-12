ca3689881374fd8ac814d6291972bd4712de8ce0
butter-cloud <130043415+butter-cloud@users.noreply.github.com>
2024-10-12 17:07:12 +0900

    Time: 0 ms (100%), Space: 41.6 MB (67.1%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        digits[digits.length - 1] += 1;
+
+        for (int i = digits.length - 1; i >= 0; i--){
+            if (digits[i] == 10 && i != 0){
+                digits[i] = 0;
+                digits[i-1] += 1;
+            }
+
+            if (digits[0] == 10){
+                int[] newDigits = new int[digits.length + 1];
+                newDigits[0] = 1;
+                return newDigits;            
+            }
+        }
+
+        return digits;
+    }
+}
