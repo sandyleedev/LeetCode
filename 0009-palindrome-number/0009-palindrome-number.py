db5602875b438544a70b1a80793c77bd30f77e27
butter-cloud <130043415+butter-cloud@users.noreply.github.com>
2024-12-02 17:57:30 +0900

    Time: 11 ms (55.01%), Space: 12 MB (6.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -10,9 +10,7 @@ class Solution(object):
         for char in string_x:
             if string_x[0] == "-":
                 return False
-            elif string_x[len(string_x) - 1] == "0":
-                if len(string_x) == 1:
-                    return True
+            elif len(string_x) != 1 and string_x[len(string_x) - 1] == "0":
                 return False
 
         reversed_x = string_x[::-1]
