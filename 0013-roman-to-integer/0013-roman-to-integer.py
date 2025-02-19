191f912db5e3dd5a0dc768cdfb6133543d098ab9
butter-cloud <130043415+butter-cloud@users.noreply.github.com>
2025-02-19 10:14:46 +0900

    Time: 0 ms (100%), Space: 17.6 MB (90.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,40 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # IV, IX, XL, XC, CD, CM
-        if "IV" in s:
-            s = s.replace("IV", "IIII")
-
-        if "IX" in s:
-            s = s.replace("IX", "VIIII")
-
-        if "XL" in s:
-            s = s.replace("XL", "XXXX")
-
-        if "XC" in s:
-            s = s.replace("XC", "LXXXX")
-
-        if "CD" in s:
-            s = s.replace("CD", "CCCC")
-
-        if "CM" in s:
-            s = s.replace("CM", "DCCCC")
+        d = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
 
         sum = 0
-
-        for c in s:
-            if c == "I":
-                sum += 1
-            if c == "V":
-                sum += 5
-            if c == "X":
-                sum += 10
-            if c == "L":
-                sum += 50
-            if c == "C":
-                sum += 100
-            if c == "D":
-                sum += 500
-            if c == "M":
-                sum += 1000
+        for i in range(len(s)):
+            if i + 1 < len(s) and d[s[i]] < d[s[i+1]]:
+                sum -= d[s[i]]
+            else:
+                sum += d[s[i]]
 
         return sum
