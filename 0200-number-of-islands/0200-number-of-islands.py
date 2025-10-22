1de42210d79adfcd4a9984aec88694df690c3b66
Sandy Lee <130043415+sandyleedev@users.noreply.github.com>
2025-10-22 21:42:51 +0100

    Time: 217 ms (80.24%), Space: 27.7 MB (80.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,27 +5,26 @@ class Solution(object):
         :rtype: int
         """
         
-        rows, cols = len(grid), len(grid[0])    # 가로줄, 세로줄 개수
         count = 0
+        rows, cols = len(grid), len(grid[0])
 
         def dfs(r, c):
             if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
                 return
             
-            # check visit
+            # visit check
             grid[r][c] = "0"
 
-            dfs(r + 1, c)
             dfs(r - 1, c)
-            dfs(r, c + 1)
+            dfs(r + 1, c)
             dfs(r, c - 1)
-
+            dfs(r, c + 1)
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
                     dfs(r, c)
                     count += 1
-
+        
         return count
 
