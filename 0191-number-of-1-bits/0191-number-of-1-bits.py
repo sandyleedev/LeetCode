8188684530737b6582feb59533bcfa82324cd510
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2025-11-19 00:32:49 +0000

    Time: 0 ms (100%), Space: 17.9 MB (31.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -3,9 +3,7 @@ class Solution:
         cnt = 0
 
         while n:
-            if n % 2 == 1:
-                cnt += 1
-                n -= 1
-            n //= 2
+            cnt += n % 2
+            n = n >> 1
         
         return cnt
