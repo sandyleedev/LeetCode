b210451c90654fddf49b2a0ef735edaab28a9bf7
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2025-12-07 01:09:59 +0000

    Time: 5 ms (3.57%), Space: 18 MB (45.95%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,28 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) == 1:
+            return 1
+
+        s = ""
+        count = 1
+
+        for i in range(1, len(chars)):
+            if chars[i - 1] != chars[i]:
+                print(f"{chars[i-1]} not equal to {chars[i]}")
+                s += chars[i - 1]
+                if count > 1:
+                    s += str(count)
+                    count = 1       # reset
+            else:
+                count += 1
+
+            # last char
+            if i == len(chars) - 1:
+                s += chars[i]
+                if count > 1:
+                    s += str(count)
+
+        for i, c in enumerate(s):
+            chars[i] = c
+
+        return len(s)
