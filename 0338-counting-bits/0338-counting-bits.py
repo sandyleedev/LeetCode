55c034c725d552adb4ca8f40ac844afa02cb66b4
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2025-12-27 17:40:09 -0600

    Time: 3 ms (94.81%), Space: 18.2 MB (92.18%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,12 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # how many 1 when n in binary representation
-        dp = [0] * (n + 1)
-        offset = 1
-
+        ans = [0] * (n + 1)
         for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i
-            dp[i] = 1 + dp[i - offset]
-        
-        return dp
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
