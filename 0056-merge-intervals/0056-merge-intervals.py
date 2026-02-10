3b039ef6f601e78fd315d260bcc44b02f98052b0
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-02-10 17:52:03 +0000

    Time: 0 ms (100%), Space: 23.2 MB (25.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,23 +1,13 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        
-        if len(intervals) == 1:
-            return intervals
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda i: i[0])
+        output = [intervals[0]]
 
-        s = sorted(intervals, key = lambda x: x[0])
-        merged = [s[0]]
-
-        for i in range(1, len(s)):
-            prev = merged[-1]       # 마지막으로 병합된 구간
-            curr = s[i]
-
-            if curr[0] <= prev[1]:
-                prev[1] = max(prev[1], curr[1])
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
             else:
-                merged.append(curr)
-        
-        return merged
+                output.append([start, end])
+
+        return output
