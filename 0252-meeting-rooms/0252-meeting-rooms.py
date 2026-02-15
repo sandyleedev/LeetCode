6d1a3a0deee01846bb7957e5fea7cbc634c26b09
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-02-15 17:06:28 +0000

    Time: 3 ms (73.41%), Space: 21.4 MB (50.12%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -5,7 +5,6 @@ class Solution:
         for i in range(1, len(intervals)):
             prev_end = intervals[i - 1][1]
             curr_start = intervals[i][0]
-
             if prev_end > curr_start:
                 return False
         return True
