5191265916f38275ac736a5754c274d613c6b365
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-06-07 09:23:40 +0100

    Time: 0 ms (100%), Space: 19.4 MB (7.18%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -10,23 +10,23 @@ class Solution:
             for i in range(left, right):
                 output.append(matrix[top][i])
             top += 1
-
+            
             # move down
             for i in range(top, bottom):
                 output.append(matrix[i][right - 1])
             right -= 1
-
+            
             if not (left < right and top < bottom):
                 break
-
+                
             # move left <-
             for i in range(right - 1, left - 1, -1):
                 output.append(matrix[bottom - 1][i])
             bottom -= 1
-
-            # move upwards
+            
+            # move up
             for i in range(bottom - 1, top - 1, -1):
                 output.append(matrix[i][left])
             left += 1
-        
+            
         return output
