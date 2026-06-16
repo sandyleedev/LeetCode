ce5f1ea354e516581bfe42fd6b1a683d72a60e5f
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-06-16 09:15:35 +0100

    Time: 147 ms (25.36%), Space: 19.7 MB (50.52%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,9 +1,5 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # allow to perform swapping up to k times
-
-        # what is the longest repeating char after k time operations
-
         count = {}
         res = 0
 
@@ -14,6 +10,7 @@ class Solution:
             while (r - l + 1) - max(count.values()) > k:
                 count[s[l]] -= 1
                 l += 1
-
+            
             res = max(res, r - l + 1)
+        
         return res
