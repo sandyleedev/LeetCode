843e9762d0aac1021176a200ae60880ab424ab1d
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-06-21 10:29:04 +0100

    Time: 67 ms (84.38%), Space: 19.6 MB (62.27%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -8,18 +8,15 @@ class Codec:
     
     def decode(self, s: str) -> List[str]:
         res = []
-        i = 0       # current delimeter index
-        
+        i = 0
+        # 5#hello4#code
         while i < len(s):
             j = i
             while s[j] != "#":
-                j += 1                
-            # char is not #
+                j += 1
             length = int(s[i:j])
-            res.append(s[j+1:j+1+length])
+            word = s[j+1:j+1+length]
+            res.append(word)
             i = j + length + 1
         return res
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+            
