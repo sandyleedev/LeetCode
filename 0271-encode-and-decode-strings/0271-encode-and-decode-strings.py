32e42d5ade58b401f39b090e2a22ff7338e6d8b1
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-06-21 10:31:19 +0100

    Time: 64 ms (91.38%), Space: 19.5 MB (62.27%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -10,13 +10,15 @@ class Codec:
         res = []
         i = 0
         # 5#hello4#code
+        
         while i < len(s):
             j = i
             while s[j] != "#":
                 j += 1
-            length = int(s[i:j])
-            word = s[j+1:j+1+length]
-            res.append(word)
-            i = j + length + 1
-        return res
             
+            # if j == "#"
+            length = int(s[i:j])
+            res.append(s[j+1:j+1+length])
+            i = j + 1 + length
+        
+        return res
