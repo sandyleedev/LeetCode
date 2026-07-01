745d78bbb65c3d4d839c248b43c8a9d2bcbb6cfb
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-01 01:49:54 +0100

    Time: 59 ms (10.52%), Space: 19.4 MB (30.61%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -2,8 +2,7 @@ from functools import lru_cache
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = {}
-        dp[0] = 1
+        dp = {0:1}
 
         for i in range(1, target + 1):
             dp[i] = 0
