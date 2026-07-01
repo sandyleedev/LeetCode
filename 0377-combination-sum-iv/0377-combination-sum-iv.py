c8d49b0baca52f1941639307a5b581bd1f1eff95
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-01 09:16:48 +0100

    Time: 57 ms (16.57%), Space: 19.2 MB (91.12%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -2,17 +2,8 @@ from functools import lru_cache
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        # ways of making 10
-        # -> dp[9] + dp[8] + dp[5]
-        # -> dp[10 - 1] + dp[10 - 2] + dp[10 -5]
-
-        # for num in nums
-        # -> dp[10 - num]
-
         dp = {}
         dp[0] = 1
-        # ...
-        # dp[target]
 
         for i in range(1, target + 1):
             dp[i] = 0
