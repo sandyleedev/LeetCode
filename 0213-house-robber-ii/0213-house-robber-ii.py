d4d921bc534128c6cbfd08dbbe6e925ab8bac805
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-06 09:06:55 +0100

    Time: 0 ms (100%), Space: 19.3 MB (31.79%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -2,13 +2,15 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) <= 1:
             return sum(nums)
-
-        def helper(houses):
+        
+        def helper(start, end):
             prev2, prev1 = 0, 0
-            for h in houses:
-                temp = max(prev2 + h, prev1)
+
+            for i in range(start, end + 1):
+                temp = max(prev2 + nums[i], prev1)
                 prev2 = prev1
                 prev1 = temp
+            
             return prev1
         
-        return max(helper(nums[1:]), helper(nums[:-1]))
+        return max(helper(0, len(nums) - 2), helper(1, len(nums) - 1))
