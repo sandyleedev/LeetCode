5563b3990abcde89c8ad594cb3fbfa370bd3162f
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-07 09:14:05 +0100

    Time: 0 ms (100%), Space: 19.1 MB (92.05%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,14 +4,16 @@ class Solution:
         dp = [0] * (n + 1)
 
         dp[0] = 1
-        
+
         for i in range(1, n + 1):
             if s[i - 1] != "0":
-                dp[i] += dp[i - 1]
-
+                dp[i] = dp[i - 1]
+            
             if i >= 2:
-                temp = int(s[i - 2: i])
+                temp = int(s[i - 2:i])
                 if 10 <= temp <= 26:
                     dp[i] += dp[i - 2]
         
         return dp[n]
+            
+
