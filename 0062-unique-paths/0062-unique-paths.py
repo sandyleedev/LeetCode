331cd2a3288f8bf1fd02f76e6b925894742286ff
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-09 03:35:43 +0100

    Time: 0 ms (100%), Space: 19.4 MB (41.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,29 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # down (m - 1) time(s)
-        # right (n - 1) time(s)
-
-        # down 2 times
-        # right 6 times
-
-        # [] [] [] [] [] [] [] []
-
-        # (8 * 7) / 2
-
-        # down down
-
-        # 56 / 2 = 28
-
-        # =================
-        # [] [] []
-        # down 2 times
-        # right 1 time
-
-        # (3 * 2) / 2
-
-
         total_slots = (m - 1) + (n - 1)
-        print(total_slots)
 
         mul = 1
         for i in range(0, m - 1):
@@ -31,7 +8,3 @@ class Solution:
             mul //= (i + 1)
 
         return mul
-
-
-
-
