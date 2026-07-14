621492390a8e15143fb7b1f2bccad2a88fa396b4
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-14 08:55:27 +0100

    Time: 47 ms (15.47%), Space: 20.7 MB (75.83%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -6,37 +6,30 @@ class Solution:
         num_rows, num_cols = len(heights), len(heights[0])
         p_queue, a_queue = deque(), deque()
 
-        # initialise
-        for i in range(num_rows):
-            p_queue.append((i, 0))
-            a_queue.append((i, num_cols - 1))
-
-        for i in range(num_cols):
-            p_queue.append((0, i))
-            a_queue.append((num_rows - 1, i))
+        for r in range(num_rows):
+            p_queue.append((r, 0))
+            a_queue.append((r, num_cols - 1))
+        
+        for c in range(num_cols):
+            p_queue.append((0, c))
+            a_queue.append((num_rows - 1, c))
         
         def bfs(queue):
             reachable = set()
             while queue:
                 (row, col) = queue.popleft()
                 reachable.add((row, col))
-
-                for (x,y) in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                for (x,y) in [(1,0), (0,1), (-1, 0), (0, -1)]:
                     new_row, new_col = row + x, col + y
-                    # 1) check if this is out of bound
                     if new_row < 0 or new_row >= num_rows or new_col < 0 or new_col >= num_cols:
                         continue
-                    # 2) check if this is already in the set
                     if (new_row, new_col) in reachable:
                         continue
-                    # 3) check if this coordinate is not higher or equal (lower than the current cell)
                     if heights[new_row][new_col] < heights[row][col]:
                         continue
-                    # 4) if it's not, we can add it to the reachable set!
                     queue.append((new_row, new_col))
             return reachable
 
         p_reachable = bfs(p_queue)
         a_reachable = bfs(a_queue)
-
         return list(p_reachable.intersection(a_reachable))
