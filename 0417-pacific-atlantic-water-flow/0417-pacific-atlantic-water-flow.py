36a425ed3770f549df86d2fa98345275a3be4b06
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-14 08:45:16 +0100

    Time: 55 ms (11.74%), Space: 20.9 MB (66.97%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,39 +2,41 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         if not heights or not heights[0]:
             return []
-
-        num_rows, num_cols = len(heights), len(heights[0])
-
-        pac_queue = deque()
-        atl_queue = deque()
-
-        for i in range(num_rows):
-            pac_queue.append((i, 0))
-            atl_queue.append((i, num_cols - 1))
         
+        num_rows, num_cols = len(heights), len(heights[0])
+        p_queue, a_queue = deque(), deque()
+
+        # initialise
+        for i in range(num_rows):
+            p_queue.append((i, 0))
+            a_queue.append((i, num_cols - 1))
+
         for i in range(num_cols):
-            pac_queue.append((0, i))
-            atl_queue.append((num_rows - 1, i))
+            p_queue.append((0, i))
+            a_queue.append((num_rows - 1, i))
         
         def bfs(queue):
             reachable = set()
             while queue:
                 (row, col) = queue.popleft()
                 reachable.add((row, col))
-                for (x, y) in [(1,0), (0, 1), (-1, 0), (0, -1)]:
+
+                for (x,y) in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                     new_row, new_col = row + x, col + y
-                    # out of bounds checking
+                    # 1) check if this is out of bound
                     if new_row < 0 or new_row >= num_rows or new_col < 0 or new_col >= num_cols:
                         continue
+                    # 2) check if this is already in the set
                     if (new_row, new_col) in reachable:
                         continue
-                    # checking new cell is higher / equal to
+                    # 3) check if this coordinate is not higher or equal (lower than the current cell)
                     if heights[new_row][new_col] < heights[row][col]:
                         continue
+                    # 4) if it's not, we can add it to the reachable set!
                     queue.append((new_row, new_col))
             return reachable
-        
-        pac_reachable = bfs(pac_queue)
-        atl_reachable = bfs(atl_queue)
-        
-        return list(pac_reachable.intersection(atl_reachable))
+
+        p_reachable = bfs(p_queue)
+        a_reachable = bfs(a_queue)
+
+        return list(p_reachable.intersection(a_reachable))
