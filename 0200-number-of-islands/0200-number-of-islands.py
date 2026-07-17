3265e40e25f400203685d3ddcadd159b0db35335
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-07-17 02:26:36 +0100

    Time: 230 ms (91.16%), Space: 21.5 MB (72.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,22 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
         
-        count = 0
-        rows, cols = len(grid), len(grid[0])
-
-        def dfs(r, c):
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
-                return
-            
-            # visit check
-            grid[r][c] = "0"
-
-            dfs(r - 1, c)
-            dfs(r + 1, c)
-            dfs(r, c - 1)
-            dfs(r, c + 1)
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    res += 1
+        return res
         
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
-                    dfs(r, c)
-                    count += 1
-        
-        return count
+    def dfs(self, grid, r, c):
+        if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] != "1":
+            return
+        grid[r][c] = "0"
 
+        self.dfs(grid, r - 1, c)
+        self.dfs(grid, r, c - 1)
+        self.dfs(grid, r + 1, c)
+        self.dfs(grid, r, c + 1)
