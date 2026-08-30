d448c7fda53b3f1d2ba615b2cfb2fbee0532eb10
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-08-30 14:54:18 +0100

    Time: 0 ms (100%), Space: 27.8 MB (80.65%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -10,18 +10,17 @@ class Solution:
         """
         if not head or not head.next:
             return
-            
+
         slow = head
         fast = head
 
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
-        
+
         second = slow.next
         slow.next = None
 
-        # reverse the second half
         prev = None
         while second:
             next_node = second.next
@@ -29,17 +28,16 @@ class Solution:
 
             prev = second
             second = next_node
-        
+
         second = prev
 
         first = head
         while second:
             tmp1 = first.next
             tmp2 = second.next
-
+            
             first.next = second
             second.next = tmp1
 
             first = tmp1
             second = tmp2
-            
