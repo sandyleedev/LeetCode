9c36951682e0c1cc1d701508c97fb14a02762ac0
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-09-19 23:26:41 +0900

    Time: 7 ms (51.91%), Space: 20.2 MB (16.37%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,17 +1,17 @@
 class Solution:
     def jump(self, nums: list[int]) -> int:
         current_end = 0
+        jumps = 0
         farthest = 0
-        total_jump = 0
 
         for i in range(len(nums)):
             farthest = max(farthest, i + nums[i])
-            
+
             if i == current_end:
                 if i == len(nums) - 1:
                     break
                 
-                total_jump += 1
+                jumps += 1
                 current_end = farthest
         
-        return total_jump
+        return jumps
