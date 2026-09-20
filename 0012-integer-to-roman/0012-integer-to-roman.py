d0c9fd623b26c631a93d9e1e6b6544447c82bc05
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-09-20 16:24:41 +0900

    Time: 7 ms (39.17%), Space: 19.3 MB (66.28%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,59 +1,25 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         res = ""
+        rules = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        ]
 
-        while 1000 <= num:
-            num -= 1000
-            res += "M"
-        
-        if 900 <= num:
-            num -= 900
-            res += "CM"
-
-        if 500 <= num:
-            num -= 500
-            res += "D"
-        
-        if 400 <= num:
-            num -= 400
-            res += "CD"
-        
-        while 100 <= num:
-            num -= 100
-            res += "C"
-        
-        if 90 <= num:
-            num -= 90
-            res += "XC"
-        
-        if 50 <= num:
-            num -= 50
-            res += "L"
-        
-        if 40 <= num:
-            num -= 40
-            res += "XL"
-        
-        while 10 <= num:
-            num -= 10
-            res += "X"
-        
-        if 9 <= num:
-            num -= 9
-            res += "IX"
-        
-        if 5 <= num:
-            num -= 5
-            res += "V"
-        
-        if 4 <= num:
-            num -= 4
-            res += "IV"
-        
-        while 1 <= num:
-            num -= 1
-            res += "I"
+        for value, symbol in rules:
+            while num >= value:
+                num -= value
+                res += symbol
 
         return res
-
-
